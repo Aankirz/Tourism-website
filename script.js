324783314b35a54b7e9df71202e5fa319ab39feb
Aankirz <sahuankit453@gmail.com>
2022-08-10 19:04:33 +0530

    header

---
+++ script.js
@@ -0,0 +1,6 @@
+let searchBtn=document.querySelector('#search-btn');
+let searchBar=document.querySelector('#search-bar-container');
+
+searchBtn.addEventListener('click',()=>{
+    searchBtn.classList.toggle('active');
+})
